4b0063e0c6cd3bfa2248e23ad61150013aba162a
Nithisrizoya <nithiraja916@gmail.com>
2026-06-17 16:21:10 +0530

    Add even or odd predictor code

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-﻿print("nithishri is the one the best employee in the salem branch by passing everyone in the team ")
+﻿def check_even_odd(number):
+    """Check if a number is even or odd"""
+    if number % 2 == 0:
+        return f"{number} is Even"
+    else:
+        return f"{number} is Odd"
+
+# Main program
+if __name__ == "__main__":
+    print("Even or Odd Predictor")
+    print("-" * 40)
+    
+    try:
+        num = int(input("Enter a number: "))
+        result = check_even_odd(num)
+        print(result)
+    except ValueError:
+        print("Please enter a valid integer!")
